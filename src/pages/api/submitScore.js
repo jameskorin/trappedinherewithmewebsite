552d56a22bd74b2905bb211f23f6bb365bca867c
James Korin <jameskorin@Jamess-MacBook-Air.local>
2023-09-01 13:53:20 -0400

    client id

--- src/pages/api/submitScore.js
+++ src/pages/api/submitScore.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
             key: process.env.STEAM_PUBLISHER_KEY,
             appid: 2584310,
             ticket: token,
-            identity: ''
+            identity: 'gameclient'
         }
     });
     console.log(v);
