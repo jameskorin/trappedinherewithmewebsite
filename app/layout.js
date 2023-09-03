4b8bee36b5507ff43595e4c6fe1732315768aeb4
James Korin <jameskorin@Jamess-MacBook-Air.local>
2023-09-03 11:14:54 -0400

    cleanup

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { ServerStyleSheet } from 'styled-components'
 import StyledComponentsRegistry from '@/lib/registry'
 
 const inter = Inter({ subsets: ['latin'] })
