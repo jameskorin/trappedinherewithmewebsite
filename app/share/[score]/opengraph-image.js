7897e3ac1257fbeb5ffb50df0ea2c1cb0a93fbda
James Korin <jameskorin@Jamess-MacBook-Air.local>
2023-09-04 12:33:49 -0400

    for real this time

--- app/share/[score]/opengraph-image.js
+++ app/share/[score]/opengraph-image.js
@@ -62,16 +62,13 @@ const outer = {
   background: '#333333',
   color: '#FFFFFF',
   width: '100%',
-  height: '100%',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  textAlign: 'center'
+  height: '100%'
 };
 const content = {
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center',
+  textAlign:'center',
   paddingTop: '0px'
 };
 const headline = {
