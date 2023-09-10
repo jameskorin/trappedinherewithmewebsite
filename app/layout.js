14d6e2ba03d4d5bc70b5f3f5eeab8f6c12b9c894
James Korin <jameskorin@Jamess-MacBook-Air.local>
2023-09-10 13:21:51 -0400

    i screwed up the import before

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import StyledComponentsRegistry from '@/lib/registry'
-import { Analytics } from '@vercel/analytics'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
